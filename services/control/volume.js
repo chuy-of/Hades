3a44eddef364288409360238603c07bd3d724d25
Chuy <chuy.of@gmail.com>
2017-06-28 23:46:06 +0000

    Updated volume comands

--- services/control/volume.js
+++ services/control/volume.js
@@ -1,11 +1,12 @@
 var exec = require('child_process').exec;
 
-// 1 : mute
-// 0 : unmute
-var value = 1;
-var cmd = ' ' + value;
+var cmdMute = 'amixer -q sset PCM toggle';
+var cmdUp = 'amixer -q sset PCM 5%+';
+var cmdDown = 'amixer -q sset PCM 5%-'; 
+
 
 exec(cmd, function(error, stdout, stderr) {
+    if (error) console.log(error);
     // command output is in stdout
     console.log(stdout);
 });
